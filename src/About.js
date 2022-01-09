0a92cb3cdfe53a91cc58aa2d2bd4159463593e10
Abin Joseph <itsmeabjs@gmail.com>
2022-01-09 18:30:06 +0530

    fix: deployment problem fix

--- src/About.js
+++ src/About.js
@@ -1,13 +1,14 @@
+// eslint-disable-next-line
 import React from "react";
 import "./About.css";
 import aboutImg from "./img/about.jpeg";
 function About() {
- // Up To Top Btn
- window.addEventListener("scroll", function(){
-  const upToTop = document.querrySelector("a.bottom__to__top");
-  upToTop.classList.toggle("active", window.scrollY > 0)
- });
- return (
+  // Up To Top Btn
+  window.addEventListener("scroll", function () {
+    const upToTop = document.querrySelector("a.bottom__to__top");
+    upToTop.classList.toggle("active", window.scrollY > 0)
+  });
+  return (
     <div className="about component__space" id="About">
       <div className="containers1">
         <div className="row">
@@ -89,16 +90,16 @@ function About() {
       {/* UP TO TOP BTN */}
       <div className="up__to__top__btn">
         <a href="#" className="bottom__to__top">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-caret-up"
-          viewBox="0 0 16 16"
-        >
-          <path d="M3.204 11h9.592L8 5.519 3.204 11zm-.753-.659 4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659z" />
-        </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-caret-up"
+            viewBox="0 0 16 16"
+          >
+            <path d="M3.204 11h9.592L8 5.519 3.204 11zm-.753-.659 4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659z" />
+          </svg>
         </a>
       </div>
     </div>
